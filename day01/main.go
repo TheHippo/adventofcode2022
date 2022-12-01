c040c33759a426223f8951362b78e2fe0852d96a
Philipp Klose <philipp.klose@paessler.com>
2022-12-01 12:18:13 +0100

    day01, part 2

--- day01/main.go
+++ day01/main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"log"
 	"os"
 	"strconv"
 )
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -15,14 +34,15 @@ func main() {
 
 	defer file.Close()
 	reader := bufio.NewScanner(file)
-	current, max := 0, 0
+	current := 0
+
+	elves := &IntHeap{}
+	heap.Init(elves)
 
 	for reader.Scan() {
 		line := reader.Text()
 		if line == "" {
-			if current > max {
-				max = current
-			}
+			heap.Push(elves, current)
 			current = 0
 		} else {
 			parsed, err := strconv.ParseInt(line, 10, 64)
@@ -36,5 +56,13 @@ func main() {
 	if err := reader.Err(); err != nil {
 		log.Fatal(err)
 	}
+	max := 0
+
+	for i := 0; i < 3; i++ {
+		// log.Println(heap.Pop(elves))
+		max += heap.Pop(elves).(int)
+	}
+
 	log.Println(max)
+
 }
